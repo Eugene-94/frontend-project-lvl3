1c5e808c37e274c198c04b631986b28de13a6950
Eugene Kaygorodov <gekak94@gmail.com>
2020-06-21 12:24:04 +0500

    webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap';
+import './styles/styles.scss';
+
 const array = ['first', 'second'];
 
 const [first, second] = array;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
